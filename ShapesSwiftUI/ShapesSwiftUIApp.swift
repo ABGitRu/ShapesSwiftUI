22d83ad9972a560fb08e73ef6d5f103611f30635
Alexey Barinov <begoodbebad2012@gmail.com>
2021-06-17 20:28:16 +0300

    Initial Commit

---
+++ ShapesSwiftUI/ShapesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesSwiftUIApp.swift
+//  ShapesSwiftUI
+//
+//  Created by Mac on 17.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ShapesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
